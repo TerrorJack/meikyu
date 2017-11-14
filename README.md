b0042d62b1c7554cda49a38b8e5456e317a99cc6
Shao Cheng <astrohavoc@gmail.com>
2017-11-14 16:07:43 +0800

    Build and install llvm-5 into a standalone container with ghc

---
+++ README.md
@@ -0,0 +1,11 @@
+# meikyu
+
+[![CircleCI](https://circleci.com/gh/TerrorJack/meikyu/tree/nightly.svg?style=shield)](https://circleci.com/gh/TerrorJack/meikyu)
+[![Docker Pulls](https://img.shields.io/docker/pulls/terrorjack/meikyu.svg)](https://hub.docker.com/r/terrorjack/meikyu/)
+
+A build environment for my personal Haskell projects.
+
+* Uses Alpine Linux edge version.
+* Based on [`stack`](https://docs.haskellstack.org/en/stable/README/)
+
+For an Ubuntu-based environment, checkout [rakuen](https://github.com/TerrorJack/rakuen).
---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+
+apk add --no-cache --no-progress --upgrade \
+    binutils-gold \
+    cmake \
+    doxygen \
+    g++ \
+    ninja \
+    pkgconfig \
+    py3-sphinx \
+    subversion
+
+cd /tmp
+svn co http://llvm.org/svn/llvm-project/llvm/branches/release_50 llvm --quiet
+cd llvm
+mkdir build
+cd build
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_FFI:BOOL=ON -DLLVM_USE_LINKER=gold -DLLVM_ENABLE_DOXYGEN:BOOL=ON -DLLVM_DOXYGEN_SVG:BOOL=ON -DLLVM_ENABLE_SPHINX:BOOL=ON -DSPHINX_EXECUTABLE=/usr/bin/sphinx-build-3 -DLLVM_BUILD_LLVM_DYLIB:BOOL=ON ..
+ninja
+ninja install
+cd /root
+
+apk del \
+    binutils-gold \
+    cmake \
+    doxygen \
+    g++ \
+    ninja \
+    pkgconfig \
+    py3-sphinx \
+    subversion
+rm -rf \
+    /tmp/bootstrap.sh \
+    /tmp/llvm
