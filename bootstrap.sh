1f408e3aba188ee653f9746045e216f197587733
Shao Cheng <astrohavoc@gmail.com>
2017-11-18 11:09:28 +0800

    Initiate build for ghc-8.3.20171118

--- bootstrap.sh
+++ bootstrap.sh
@@ -46,14 +46,14 @@ stack --no-terminal --resolver lts-9 --system-ghc install \
 cd /tmp
 git clone git://git.haskell.org/ghc.git
 cd ghc
-git checkout c7297342a4797ea36df8767d9c208cfb45de2d09
+git checkout 12a7444463184e9eddbe7b7251a0ee1e976f4d75
 git submodule update --init --recursive
 mv /tmp/build.mk mk/
 ./boot
-./configure --prefix=/root/.stack/programs/x86_64-linux/ghc-8.3.20171116 --disable-ld-override
+./configure --prefix=/root/.stack/programs/x86_64-linux/ghc-8.3.20171118 --disable-ld-override
 make -j4
 make install
-printf "installed" > /root/.stack/programs/x86_64-linux/ghc-8.3.20171116.installed
+printf "installed" > /root/.stack/programs/x86_64-linux/ghc-8.3.20171118.installed
 cd /root
 
 apk del ghc
