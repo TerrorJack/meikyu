4bb707958000db52c8691a7f9db620584f53b890
Shao Cheng <astrohavoc@gmail.com>
2017-11-19 18:51:38 +0800

    Fix environment variable for sphinx-build path

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ apk upgrade --no-cache --no-progress
 apk add --no-cache --no-progress \
     autoconf \
     automake \
+    bash \
     binutils-gold \
     bzip2 \
     ca-certificates \
@@ -19,6 +20,7 @@ apk add --no-cache --no-progress \
     gzip \
     libedit-dev \
     libffi-dev \
+    libtool \
     make \
     musl-dev \
     numactl-dev \
@@ -45,13 +47,14 @@ git checkout b8e324a4ff7d04d55dc8d8d8b9e441225c9dd4f6
 git submodule update --init --recursive
 mv /tmp/build.mk mk/
 ./boot
-SPHINX-BUILD=/usr/bin/sphinx-build-3 ./configure --prefix=/root/.stack/programs/x86_64-linux/ghc-8.3.20171118 --disable-ld-override
+SPHINXBUILD=/usr/bin/sphinx-build-3 ./configure --prefix=/root/.stack/programs/x86_64-linux/ghc-8.3.20171118 --disable-ld-override
 make -j4
 make install
 printf "installed" > /root/.stack/programs/x86_64-linux/ghc-8.3.20171118.installed
 cd /root
 
 apk del ghc
+update-alternatives --install /bin/sh sh /bin/bash 44
 mv /root/.stack/programs /tmp/programs
 rm -rf \
     /tmp/bootstrap.sh \
