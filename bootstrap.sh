3bf58a0697b0845413677d7a4bcae3f3455e9ab3
Shao Cheng <astrohavoc@gmail.com>
2017-12-03 13:04:37 +0800

    Update ghc-8.2.2 image

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,6 @@ apk add --no-cache --no-progress \
     binutils-gold \
     bzip2 \
     ca-certificates \
-    cmake \
     coreutils \
     file \
     findutils \
@@ -22,14 +21,12 @@ apk add --no-cache --no-progress \
     libffi-dev \
     make \
     musl-dev \
-    ninja \
     numactl-dev \
     openssh \
     patch \
     perl \
     py3-sphinx \
     sed \
-    subversion \
     tar \
     wget \
     xz \
@@ -39,7 +36,7 @@ mkdir -p /root/.local/bin
 cd /root/.local/bin
 mv /tmp/cabal .
 chmod u+x cabal
-tar xz --wildcards --strip-components=1 -C . '*/stack' -f /tmp/stack-1.6.0.20171022-linux-x86_64-static.tar.gz
+tar xz --wildcards --strip-components=1 -C . '*/stack' -f /tmp/stack-1.6.0.20171202-linux-x86_64-static.tar.gz
 stack --no-terminal --resolver lts-9 --system-ghc install \
     alex \
     happy \
@@ -48,32 +45,22 @@ stack --no-terminal --resolver lts-9 --system-ghc install \
 cd /tmp
 git clone git://git.haskell.org/ghc.git
 cd ghc
-git checkout bc761ad9c65c7aa62d38db39c59a6c0ae59c8ab8
+git checkout ghc-8.2.2-release
 git submodule update --init --recursive
 mv /tmp/build.mk mk/
 ./boot
-SPHINXBUILD=/usr/bin/sphinx-build-3 ./configure --prefix=/root/.stack/programs/x86_64-linux/ghc-8.3.20171201 --disable-ld-override
+SPHINXBUILD=/usr/bin/sphinx-build-3 ./configure --prefix=/root/.stack/programs/x86_64-linux/ghc-8.2.2 --disable-ld-override
 make -j4
 make install
-printf "installed" > /root/.stack/programs/x86_64-linux/ghc-8.3.20171201.installed
+printf "installed" > /root/.stack/programs/x86_64-linux/ghc-8.2.2.installed
 apk del ghc
-
-cd /tmp
-svn co http://llvm.org/svn/llvm-project/llvm/branches/release_50 llvm --quiet
-cd llvm
-mkdir build
-cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_FFI:BOOL=ON -DLLVM_USE_LINKER=gold -DLLVM_ENABLE_SPHINX:BOOL=ON -DSPHINX_EXECUTABLE=/usr/bin/sphinx-build-3 ..
-ninja install
-apk del subversion
-cd /root
 
 mv /root/.stack/programs /tmp/programs
 rm -rf \
     /tmp/bootstrap.sh \
     /tmp/ghc \
     /tmp/llvm \
-    /tmp/stack-1.6.0.20171022-linux-x86_64-static.tar.gz \
+    /tmp/stack-1.6.0.20171202-linux-x86_64-static.tar.gz \
     /root/.local/bin/HsColour \
     /root/.local/bin/alex \
     /root/.local/bin/happy \
